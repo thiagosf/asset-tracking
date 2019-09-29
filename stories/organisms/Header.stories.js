dfa67fcfd0580bf49b3f6e9be8acc7010ac166b7
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-09-29 15:47:29 -0300

    Header done

--- stories/organisms/Header.stories.js
+++ stories/organisms/Header.stories.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf, addDecorator } from '@storybook/react'
+import { storiesOf } from '@storybook/react'
 import { Header } from '../../src/components/organisms'
 
 const stories = storiesOf('molecules/Header', module)
