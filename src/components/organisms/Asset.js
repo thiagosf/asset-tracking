f325f6aeddfb38a776e6d4d28ac21be7fa04fd42
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-10-02 08:52:47 -0300

    WIP asset

--- src/components/organisms/Asset.js
+++ src/components/organisms/Asset.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Button } from '../atoms'
 
 export default class Asset extends Component {
   static propTypes = {
@@ -8,8 +9,10 @@ export default class Asset extends Component {
 
   render () {
     const { asset } = this.props
+    const lastVariation = this._getLastVariation()
+    const classes = this._getClasses()
     return (
-      <div className="asset">
+      <div className={classes}>
         <header className="asset__header">
           <div className="asset__name_and_age">
             <div className="asset__name">
@@ -23,16 +26,61 @@ export default class Asset extends Component {
             {asset.broker}
           </div>
         </header>
-        <div className="asset__amount">
-          {asset.amount}
+        <div className="asset__total-and-last-variation">
+          <div className="asset__total">
+            {asset.total}
+          </div>
+          <div className="asset__last-variation">
+            {lastVariation}
+          </div>
         </div>
-        <div className="asset__total">
-          {asset.total}
-        </div>
-        <div className="asset__currency">
-          {asset.currency}
+        {asset.amount !== asset.total &&
+          <div className="asset__amount-and-currency">
+            <span className="asset__amount">{asset.amount}</span> <span className="asset__currency">{asset.currency}</span>
+          </div>
+        }
+        <div className="asset__actions">
+          <Button
+            intent='primary'
+            size='small'
+            onPress={() => console.log('timeline')}
+          >Timeline</Button>
+          <Button
+            intent='success'
+            size='small'
+            onPress={() => console.log('new info')}
+          >New info</Button>
+          <Button
+            intent='danger'
+            size='small'
+            onPress={() => console.log('danger')}
+          >Remove</Button>
         </div>
       </div>
     )
+  };
+
+  _getLastVariation = () => {
+    const { variations } = this.props.asset
+    if (variations.length > 0) {
+      const lastIndex = variations.length - 1
+      return variations[lastIndex].variation
+    }
+    return '?'
+  };
+
+  _getClasses = () => {
+    let classes = ['asset']
+    const { variations } = this.props.asset
+    if (variations.length > 0) {
+      const lastIndex = variations.length - 1
+      const lastVariation = variations[lastIndex].variation
+      if (lastVariation > 0) {
+        classes.push('asset--positive')
+      } else if (lastVariation < 0) {
+        classes.push('asset--negative')
+      }
+    }
+    return classes.join(' ')
   };
 }
--- stories/organisms/Asset.stories.js
+++ stories/organisms/Asset.stories.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text, number } from '@storybook/addon-knobs'
 import { Asset } from '../../src/components/organisms'
+import { decoratorContainer } from '../helpers'
 
 const stories = storiesOf('molecules/Asset', module)
 
+stories.addDecorator(decoratorContainer)
 stories.addDecorator(withKnobs)
 
 stories.add('default', () => {
-  const name = text('name', 'Tesouro IPCA +2045')
-  const broker = text('broker', 'Easynvest')
   const variation1 = number('variation1', 3.2)
   const variation2 = number('variation2', 2.9)
   const variation3 = number('variation3', 4.9)
@@ -23,17 +23,20 @@ stories.add('default', () => {
     date: '2019-05-01',
     variation: variation3
   }]
+  const name = text('name', 'Tesouro IPCA +2045')
+  const broker = text('broker', 'Easynvest')
   const amount = number('amount', 12102.54)
   const total = number('total', 12102.54)
   const currency = text('currency', 'BRL')
   const age = number('age (in days)', 365)
   const annualProfitability = number('annual profitability', 4.4)
   const asset = {
+    variations,
     name,
     broker,
-    variations,
     amount,
     total,
+    currency,
     age,
     annual_profitability: annualProfitability
   }
